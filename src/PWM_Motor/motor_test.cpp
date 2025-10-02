72c8041e49c5a6b9d0c686fc66a272f5f7bd468e
YJChen <yj.chen.working@gmail.com>
2025-10-02 23:30:09 +0800

    update

--- src/PWM_Motor/motor_test.cpp
+++ src/PWM_Motor/motor_test.cpp
@@ -1,16 +1,22 @@
 #include <Arduino.h>
-
+/*
 const int pwmPin = 23;     // PWM 控制腳
 const int dirPin1 = 36;    // 方向控制腳1
 const int dirPin2 = 37;    // 方向控制腳2
-const int maxSpeed = 150;  // 最大速度
-const int stepDelay = 10;  // 加速/減速延遲(ms)
+*/
+const int pwmPin = 2;     // PWM 控制腳
+const int dirPin1 = 3;    // 方向控制腳1
+const int dirPin2 = 4;    // 方向控制腳2
+
+const int maxSpeed = 100;  // 最大速度
+const int stepDelay = 100;  // 加速/減速延遲(ms)
 
 void setup() {
   pinMode(pwmPin, OUTPUT);
   pinMode(dirPin1, OUTPUT);
   pinMode(dirPin2, OUTPUT);
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   // 一開始停住
   digitalWrite(dirPin1, LOW);
   digitalWrite(dirPin2, LOW);
@@ -21,7 +27,9 @@ void loop() {
   // 正轉加速
   digitalWrite(dirPin1, HIGH);
   digitalWrite(dirPin2, LOW);
-  for(int speed = 0; speed <= maxSpeed; speed++) {
+  for(int speed = 10; speed <= maxSpeed; speed++) {
+    Serial.print("+");
+    Serial.println(speed);
     analogWrite(pwmPin, speed);
     delay(stepDelay);
   }
@@ -35,7 +43,9 @@ void loop() {
   // 反轉加速
   digitalWrite(dirPin1, LOW);
   digitalWrite(dirPin2, HIGH);
-  for(int speed = 0; speed <= maxSpeed; speed++) {
+  for(int speed = 10; speed <= maxSpeed; speed++) {
+    Serial.print("-");
+    Serial.println(speed);
     analogWrite(pwmPin, speed);
     delay(stepDelay);
   }
