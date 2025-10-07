c888ebf645b0ab5676c1f47f7067776872217a37
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2025-10-07 15:36:16 +0800

    Update Robot.h

--- include/Robot.h
+++ include/Robot.h
@@ -31,44 +31,6 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 struct GyroData {float heading = 0.0; bool valid = false;} gyroData;
 struct LineData {int data = 999; bool valid = false;} lineData;
 struct BallData {int data = 999; bool valid = false;} ballData;
-
-
-void linesensor(){
-    lineData.data = 999;
-}
-
-void ballsensor(){
-    ballData.data = 999;
-}
-   
-void showStart() {
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setCursor(0, 20);
-  display.println("Start");
-  display.display();
-}
-
-
-void showRunScreen() {
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setCursor(0, 20);
-  display.println("Run");
-  display.display();
-}
-
-
-void showSensors(float gyro, int light, int ball) {
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setCursor(0, 0);  display.print("Gyro: ");  display.println(gyro);
-  display.setCursor(0, 15); display.print("Light: "); display.println(light);
-  display.setCursor(0, 30); display.print("Ball: ");  display.println(ball);
-  display.display();
-}
-
-
 /*
 void readBNO085Yaw(HardwareSerial &serial) {
   const int PACKET_SIZE = 19;
@@ -117,85 +79,6 @@ void Robot_Init(){
   pinMode(DIRB_4,OUTPUT);
 }
 
-void readBNO085Yaw() {
-  const int PACKET_SIZE = 19;
-  uint8_t buffer[PACKET_SIZE];
-
-
-  while (Serial2.available() >= PACKET_SIZE) {
-    buffer[0] = Serial2.read();
-    if (buffer[0] != 0xAA) continue;
-    buffer[1] = Serial2.read();
-    if (buffer[1] != 0xAA) continue;
-
-
-    for (int i = 2; i < PACKET_SIZE; i++) buffer[i] = Serial2.read();
-
-
-    int16_t yaw_raw = (int16_t)((buffer[4] << 8) | buffer[3]);
-    gyroData.valid = true;
-    gyroData.heading = yaw_raw * 0.01f; // 轉成度
-    break;
-  }
-}
-void SetMotorSpeed(uint8_t port, int8_t speed){
-  speed = constrain(speed,-100,100);
-  int pwmVal = abs(speed) * 255 / 100;
-  switch (port){
-    case 1:
-      analogWrite(pwmPin1, pwmVal);
-      if(speed>0){
-        digitalWrite(DIRA_1,HIGH);
-        digitalWrite(DIRB_1,LOW);
-      } else if(speed<0){
-        digitalWrite(DIRA_1,LOW);
-        digitalWrite(DIRB_1,HIGH);
-      } else{
-        digitalWrite(DIRA_1,LOW);
-        digitalWrite(DIRB_1,LOW);
-      }
-      break;
-    case 2:
-      analogWrite(pwmPin2, pwmVal);
-      if(speed>0){
-        digitalWrite(DIRA_2,HIGH);
-        digitalWrite(DIRB_2,LOW);
-      } else if(speed<0){
-        digitalWrite(DIRA_2,LOW);
-        digitalWrite(DIRB_2,HIGH);
-      } else{
-        digitalWrite(DIRA_2,LOW);
-        digitalWrite(DIRB_2,LOW);
-      }
-      break;
-    case 3:
-      analogWrite(pwmPin3, pwmVal);
-      if(speed>0){
-        digitalWrite(DIRA_3,HIGH);
-        digitalWrite(DIRB_3,LOW);
-      } else if(speed<0){
-        digitalWrite(DIRA_3,LOW);
-        digitalWrite(DIRB_3,HIGH);
-      } else{
-        digitalWrite(DIRA_3,LOW);
-        digitalWrite(DIRB_3,LOW);
-      }
-      break;
-    case 4:
-      analogWrite(pwmPin4, pwmVal);
-      if(speed>0){
-        digitalWrite(DIRA_4,HIGH);
-        digitalWrite(DIRB_4,LOW);
-      } else if(speed<0){
-        digitalWrite(DIRA_4,LOW);
-        digitalWrite(DIRB_4,HIGH);
-      } else{
-        digitalWrite(DIRA_4,LOW);
-        digitalWrite(DIRB_4,LOW);
-      }
-      break;
-  }
-}
 
 void readBNO085Yaw() {
   const int PACKET_SIZE = 19;
